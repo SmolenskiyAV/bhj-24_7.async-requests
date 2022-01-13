bbbba77bcf05a2002ef9e11f46ef0ad54fbd92e0
SmolenskiyAV <JoJo_666@list.ru>
2022-01-14 03:12:51 +0700

    Версия 0.3 Не работает парсинг в первой задаче

--- preloader/task.js
+++ preloader/task.js
@@ -1,19 +1,82 @@
 // ### Task 7.1 ###
 
-const imgLoader = document.getElementById('loader');
+let storage;
+alert('Содержимое хранилища: ' + localStorage.getItem('savedResponse')); // тестовый вывод извлечённых из LocalStorage данных
 
-async function getCurrency() {
+
+
+const imgLoader = document.getElementById('loader');  // элемент "gif-анимация загрузки"
+const items = document.getElementById('items'); // контейнер, в котором содержится список валют
+
+async function getCurrency() {  // асинхронная функция получения данных списка валют с сервера
   let response = await fetch('https://netology-slow-rest.herokuapp.com/');
 
-  if (response.ok) {  
-    let data = await response.json(); // получаем данные с сервера в виде строки JSON
-    console.dir(JSON.parse(data)); // Почему не парсится data? Она ведь JSON.. Мне нужен объект. Как его из data получить?
-    imgLoader.classList.remove('loader_active');    
-    return data;
+  if (response.ok) {  // если ответ от сервера успешный
+    let data = await response.json(); // получаем данные с сервера в виде объекта
+    /*
+    let currencyList = Object.keys(data.response.Valute); // массив списка (названий ключей) валют из объекта, полученного по запросу к серверу
+    let currencyListLength =  currencyList.length; // размер списка валют (длина массива)
+    */
+    imgLoader.classList.remove('loader_active');  // скрываем анимацию на странице
+
+    let dataArr = ['{', '}']; // пустой массив для строковых занчений списка валют
+
+    for (let key in data.response.Valute) { // перебор свойств (списка валют) объекта, полученного по запросу к серверу
+      let i = 1;
+      let value = data.response.Valute[key].Value; // численное значение валюты из полученного списка 
+      let charCode = data.response.Valute[key].CharCode; // название валюты из полученного списка
+
+      items.insertAdjacentHTML('beforeend', `
+      <div class="item">
+        <div class="item__code">
+          ${charCode}
+        </div>
+        <div class="item__value">
+          ${value}
+        </div>
+        <div class="item__currency">
+          руб.
+        </div>
+      </div>`); // добавление нового DOM-элемента, являющегося блоком списка валют
+
+      dataArr.splice(i, 0, `${charCode}`, ':', `${value}`, ','); // заполнение строкового массива
+      i++;
+    };
+
+       localStorage.setItem('savedResponse', dataArr.join('')); //запись полученного списка валют в виде строки в LocalStorage
+
+    return data; //вывод из функции значения списка всех валют в виде JSON объекта
   } else {
     alert('error', response.status);    
   };
 };
 
-getCurrency();
+
+// вызов асинхронной функции для загрузки с сервера списка валют. Отображение  этого списка на странице
+getCurrency(); 
+
+
+
+document.addEventListener('click', () => { // тестовая функция для очистки хранилища
+  delete localStorage.savedResponse;
+  alert('Локальное хранилище очищено!')
+});
+
+
+document.addEventListener('keydown', (event) => { // тестова функция запуска парсинга строки, полученной из ответа на запрос серверу
+
+  pressedSymbol = event.key; // получение символа по нажатию на кнопку клавиатуры
+  
+  if ((pressedSymbol === 'j') && (localStorage.savedResponse !== null)) { // если нажата "j" и хранилище не пустое, начинаем парсинг
+  console.dir('ОБЪЕКТ JSON:  ' + JSON.parse(localStorage.getItem('savedResponse'))); // На этот раз аргумент парсинга - стопудовая строка с правильными символами в начале { и в конце } через запятые. Пачему жеж оно не парсится???
+  console.log(event.key)
+  }
+});
+
+
+  
+
+
+
+
 
